bc8420f7f15a916faf560950f1362212fc70f783
ShiragG <gulyayevsgh@mail.ru>
2024-11-27 19:27:04 +0500

    Подправлены модели и админка

--- terminology_service/service/admin.py
+++ terminology_service/service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Catalog, CatalogVersion, CatalogElement
+
 # Register your models here.
+
+admin.site.register([Catalog,CatalogVersion,CatalogElement])
--- terminology_service/service/models.py
+++ terminology_service/service/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Catalog(models.Model):
+    code = models.CharField(max_length=100, blank=False, null=False, unique=True,help_text='')
+    name = models.CharField(max_length=300, blank=False, null=False)
+    description = models.TextField()
+
+class CatalogVersion(models.Model):
+    catalog_id = models.ForeignKey(Catalog, on_delete=models.CASCADE, blank=False, null=False)
+    version = models.CharField(max_length=50, blank=False, null=False)
+    start_date = models.DateField()
+
+class CatalogElement(models.Model):
+    catalog_version_id = models.ForeignKey(CatalogVersion, on_delete=models.CASCADE, blank=False, null=False)
+    code = models.CharField(max_length=100, blank=False, null=False)
+    value = models.CharField(max_length=300, blank=False, null=False)
