197afae3ffda48f19098ecbbf90cff8b49572b5a
ShiragG <gulyayevsgh@mail.ru>
2024-11-28 08:51:43 +0500

    Изменены зависимости в requirements

--- terminology_service/service/admin.py
+++ terminology_service/service/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 
 from .models import Catalog, CatalogVersion, CatalogElement
 
-# Register your models here.
-
 admin.site.register([Catalog,CatalogVersion,CatalogElement])
